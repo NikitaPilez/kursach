d344ae3db1d9ae6ec5ff39e4250b70ffd04e0817
Havonyuk <lesha-havronyuk@yandex.ru>
2019-04-02 16:35:25 +0300

    Test update from Alex
    Test update from Alex in welcome.blade .php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    It's kursach
                 </div>
 
                 <div class="links">
